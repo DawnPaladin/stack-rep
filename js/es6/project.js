cef06c85e9ab8a4fdccc27efe0e3917b952e5004
James Harris <james.harris@mfwbooks.com>
2015-12-11 15:34:01 -0600

    Switch to StackOverflow. Render avatar, reputation, and badge count.

--- js/es6/project.js
+++ js/es6/project.js
@@ -1,7 +1,11 @@
-var userID = 3803;
-$.getJSON(`https://api.stackexchange.com/2.2/users/${userID}?site=graphicdesign`, function(json) {
-	console.log(json);
+var userID = 1805453;
+$.getJSON(`https://api.stackexchange.com/2.2/users/${userID}?site=stackoverflow`, function(json) {
 	var userObj = json.items[0];
-	var avatar = userObj.profile_image;
-	$('body').append(`<img src=${avatar}>`);
+	console.log(userObj);
+	$('.avatar').attr('src', userObj.profile_image);
+	$('#rep-badge').text(userObj.reputation);
+	var badgesHTML = "<span class='gold-badge'>g</span>".repeat(userObj.badge_counts.gold) +
+		"<span class='silver-badge'>s</span>".repeat(userObj.badge_counts.silver) +
+		"<span class='bronze-badge'>b</span>".repeat(userObj.badge_counts.bronze);
+	$('#badges-badge').html(badgesHTML);
 });
